4e909308b764a0687b68e740010017d14a12ccbf
jubeda-begum <shaikjubedabegum50@gmail.com>
2025-11-30 21:01:36 +0530

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,17 @@
+let list = document.querySelector("#myList");
+let btn = document.querySelector("#addBtn");
+btn.addEventListener("click", function () {
+    let newLi = document.createElement("li");
+    newLi.innerText = "New Item";
+    let count = list.children.length + 1;
+    if (count % 2 !== 0) {
+        newLi.style.fontWeight = "bold";
+        newLi.style.color = "blue";
+    }
+    else {
+        newLi.style.fontStyle = "italic";
+        newLi.style.color = "red";
+    }
+
+    list.appendChild(newLi);
+});
